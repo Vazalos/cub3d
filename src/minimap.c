b81f920e0a5c9197ecfc018dc5241e58d20dc460
David Nuno Fernandes Roque <david-fe@c1r6s4.42porto.com>
2025-10-14 17:03:48 +0100

    minor refactoring, removed casts in minimap.c

--- src/minimap.c
+++ src/minimap.c
@@ -6,19 +6,21 @@
 /*   By: david-fe <david-fe@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 16:52:41 by david-fe          #+#    #+#             */
-/*   Updated: 2025/10/14 16:37:23 by david-fe         ###   ########.fr       */
+/*   Updated: 2025/10/14 17:01:03 by david-fe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void init_minimap(t_data *data)
+void	init_minimap(t_data *data)
 {
 	data->mmap.scale = 10;
 	data->mmap.view_radius = 7;
 	data->mmap.view_size = (2 * data->mmap.view_radius) + 1;
-	data->mmap.center_x = (data->mmap.scale * data->mmap.view_radius) + data->mmap.scale;
-	data->mmap.center_y = (data->mmap.scale * data->mmap.view_radius) + data->mmap.scale;
+	data->mmap.center_x = (data->mmap.scale * data->mmap.view_radius)
+		+ data->mmap.scale;
+	data->mmap.center_y = (data->mmap.scale * data->mmap.view_radius)
+		+ data->mmap.scale;
 	data->mmap.start_x = (int)data->cast.pov_x - data->mmap.view_radius;
 	data->mmap.start_y = (int)data->cast.pov_y - data->mmap.view_radius;
 	data->mmap.end_x = (int)data->cast.pov_x + data->mmap.view_radius;
@@ -29,48 +31,45 @@ void init_minimap(t_data *data)
 
 void	draw_minimap(t_data *data)
 {
-	int x;
-	int y;
+	int	x;
+	int	y;
 
 	data->mmap.start_x = (int)data->cast.pov_x - data->mmap.view_radius;
 	data->mmap.start_y = (int)data->cast.pov_y - data->mmap.view_radius;
 	data->mmap.end_x = (int)data->cast.pov_x + data->mmap.view_radius;
 	data->mmap.end_y = (int)data->cast.pov_y + data->mmap.view_radius;
-
-	y = data->mmap.start_y;
-	while(y < data->mmap.end_y)
+	y = data->mmap.start_y - 1;
+	while (++y < data->mmap.end_y)
 	{
-		x = data->mmap.start_x;
-		while(x < data->mmap.end_x) 
+		x = data->mmap.start_x - 1;
+		while (++x < data->mmap.end_x)
 		{
-			if (x >= 0 && y >= 0 && (int)x <= data->map->max_x[y]
-				&& (int)y < data->map->max_y && data->map->map[(int)y][(int)x] == '0')
+			if (x >= 0 && y >= 0 && x < data->map->max_x[y]
+				&& y < data->map->max_y && data->map->map[y][x] == '0')
 				draw_square(data, x, y, WHITE, 1);
-			else if (x >= 0 && y >= 0 && (int)x <= data->map->max_x[(int)y]
-				&& (int)y < data->map->max_y && data->map->map[(int)y][(int)x] == '1')
+			else if (x >= 0 && y >= 0 && x < data->map->max_x[y]
+				&& y < data->map->max_y && data->map->map[y][x] == '1')
 				draw_square(data, x, y, BLACK, 1);
-			else 
+			else
 				draw_square(data, x, y, GRAY, 1);
-			if ((int)x == (int)data->cast.pov_x && (int)y == (int)data->cast.pov_y)
+			if (x == (int)data->cast.pov_x && y == (int)data->cast.pov_y)
 			{
 				draw_square(data, x, y, GREEN, 0);
 				//if ((x % data->mmap.scale == data->mmap.scale / 2) && (y % data->mmap.scale == data->mmap.scale / 2))
 				//	draw_player_dir(data, x, y);
 			}
-			x++;
-		}	
-		y++;
+		}
 	}
 }
 
 void	draw_square(t_data *data, int x, int y, unsigned int color, int alpha)
 {
-	int i;
-	int j;
-	int offset_x;
-	int offset_y;
+	int	i;
+	int	j;
+	int	offset_x;
+	int	offset_y;
 	int	win_x;
-	int win_y;
+	int	win_y;
 
 	offset_x = x - (int)data->cast.pov_x;
 	offset_y = y - (int)data->cast.pov_y;
@@ -84,11 +83,11 @@ void	draw_square(t_data *data, int x, int y, unsigned int color, int alpha)
 		while (j < data->mmap.scale)
 		{
 			if (alpha == 0)
-				ft_draw_pixel(data, j + (int)win_x,
-					i + (int)win_y, color);
+				ft_draw_pixel(data, j + win_x,
+					i + win_y, color);
 			else
-				ft_draw_pixel(data, j + (int)win_x,
-					i + (int)win_y, get_alpha_color
+				ft_draw_pixel(data, j + win_x,
+					i + win_y, get_alpha_color
 					(data, j + win_x, 
 					i + win_y, color));
 			j++;
